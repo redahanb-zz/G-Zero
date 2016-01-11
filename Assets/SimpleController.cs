a01f1f9f6532c1334346a64d5126bb7fe21182a9
Ben Redahan <redahanb@gmail.com>
2016-01-11 23:10:42 +0000

    Initial commit

---
+++ Assets/SimpleController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleController : MonoBehaviour {
+
+	public float 	speed_M, 
+					speed_R;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			transform.position += transform.forward * speed_M * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.position -= transform.forward * speed_M * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Rotate(Vector3.up * speed_R * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Rotate(-Vector3.up * speed_R * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/StickToSurface.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToSurface : MonoBehaviour {
+	
+	RaycastHit rayHit;
+	public Vector3 surfaceNormal;
+	public float gravSpeed;
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawRay (transform.position, -transform.up*5.0f);
+
+		if (Physics.Raycast (transform.position, -transform.up, out rayHit, 5.0f) && rayHit.collider.tag == "Planetoid") 
+		{
+			surfaceNormal = rayHit.normal;
+
+			//transform.position = rayHit.point + rayHit.normal*0.5f;
+			transform.position = rayHit.point + rayHit.normal;
+			rigidBody.AddForce (-transform.up * gravSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (Vector3.Cross (transform.right, rayHit.normal), rayHit.normal), Time.deltaTime * 15.0f);
+		} 
+		else 
+		{
+			rigidBody.useGravity = true;
+		}
+	}
+}
